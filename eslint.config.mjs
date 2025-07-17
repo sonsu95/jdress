5be9a488d172d5b4ce4d58823048698db4366e46
sonsu95 <leesonsu07@gmail.com>
2025-07-18 00:28:51 +0900

    chore: add shadcn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import react from 'eslint-plugin-react-x';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
-import react from 'eslint-plugin-react-x';
-import eslintConfigPrettier from 'eslint-config-prettier/flat';
-import js from '@eslint/js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
