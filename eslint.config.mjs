d3c108e75b63e2f41a290b5faa62746131834495
sonsu95 <leesonsu07@gmail.com>
2025-07-18 00:19:37 +0900

    chore: eslint && prettier setting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,46 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import react from 'eslint-plugin-react-x';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import js from '@eslint/js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
 });
 
+const ignoresConfig = {
+  ignores: [
+    'dist/**',
+    'node_modules/**',
+    'build/**',
+    '.next/**',
+    'coverage/**',
+    '*.min.js',
+    '**/*.d.ts',
+    '.history/**',
+    '**/.git/**',
+  ],
+};
+
+const customConfig = {
+  rules: {
+    '@typescript-eslint/consistent-type-imports': 'error',
+  },
+};
+
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ignoresConfig,
+  ...compat.config({
+    extends: ['next', 'next/core-web-vitals', 'next/typescript'],
+  }),
+  react.configs.recommended,
+  customConfig,
+  eslintConfigPrettier,
 ];
 
 export default eslintConfig;
